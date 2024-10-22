a7aebeef32c6dc492579e9de0efe5927aa4f8720
suhelkhanca <suhel.hata@gmail.com>
2024-10-22 19:21:52 +0530

    add user login-registration functionality

---
+++ login.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>PerfectMeal - Bootstrap Restaurant Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta content="" name="keywords" />
+    <meta content="" name="description" />
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon" />
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&family=Pacifico&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Icon Font Stylesheet -->
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
+      rel="stylesheet"
+    />
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet" />
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+    <link
+      href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css"
+      rel="stylesheet"
+    />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+	
+	<!-- Script -->
+	<script type="text/javascript">
+        		$(document).ready(function(){
+			
+			   $("#login").click(function(){
+				
+					email=$("#email").val();
+					password=$("#password").val();
+					 $.ajax({
+						type: "POST",
+						url: "pcheck.php",
+						data: "email="+email+"&password="+password,
+						success: function(html){
+						  if(html=='true')
+						  {
+							  
+							  $("#add_err2").html('<div class="alert alert-success"> \
+													<strong>Authenticated</strong> \ \
+												</div>');
+
+							window.location.href = "blog.php";
+						  
+						  } else if (html=='false') {
+								$("#add_err2").html('<div class="alert alert-danger"> \
+													<strong>Authentication</strong> failure. \ \
+												</div>');
+								
+						  
+						  } else {
+								$("#add_err2").html('<div class="alert alert-danger"> \
+													<strong>Error</strong> processing request. Please try again. \ \
+												</div>');
+						  }
+						},
+						beforeSend:function()
+						{
+                            $("#add_err2").html("loading...");
+						}
+					});
+					 return false;
+				});
+});
+    </script>
+
+  </head>
+
+  <body>
+    <div class="container-xxl bg-white p-0">
+      <!-- Spinner Start -->
+      <div
+        id="spinner"
+        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center"
+      >
+        <div
+          class="spinner-border text-primary"
+          style="width: 3rem; height: 3rem"
+          role="status"
+        >
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>
+      <!-- Spinner End -->
+      <!-- Nav Bar -->
+       <?php include_once 'nav.php' ?>
+      <!-- Hero Start -->
+      <div class="container-xxl position-relative p-0">
+        <div class="container-xxl py-5 bg-dark hero-header mb-5">
+          <div class="container text-center my-5 pt-5 pb-4">
+            <h1 class="display-3 text-white mb-3 animated slideInDown">
+              Contact Us
+            </h1>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb justify-content-center text-uppercase">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                <li
+                  class="breadcrumb-item text-white active"
+                  aria-current="page"
+                >
+                  Contact
+                </li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <!-- Hero End -->
+        <!-- Login Form -->
+         <div class="container">
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    
+					<div class="alert alert-danger">
+					<strong>You must be logged in to view the blog.</strong>
+					</div>
+					
+					<hr>
+                    <h2 class="intro-text text-center">
+                        <strong>Login</strong>
+                    </h2>
+					<div id="add_err2"></div>
+                    <hr>       
+                    <form role="form">
+                        <div class="row">
+                            <div class="form-group">
+                                <label>Email Address</label>
+                                <input type="email" id="email" name="email" maxlength="25" class="form-control">
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input type="password" id="password" name="password" maxlength="10" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" id="login" class="btn btn-primary w-100 mt-4 mb-2 py-2 ">Login</button>
+                            </div>
+                        </div>
+                    </form>
+					
+					<div class="form-group">
+						<a class="text-dark" href="register.php">Not a Member? <button type="submit" class="btn btn-primary">Register here</button></a>
+					</div>
+
+					
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+      <!-- Footer Start -->
+     <?php include_once 'footer.php' ?>
+    
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+  </body>
+</html>
---
+++ register.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8" />
+    <title>PerfectMeal - Bootstrap Restaurant Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta content="" name="keywords" />
+    <meta content="" name="description" />
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon" />
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&family=Pacifico&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Icon Font Stylesheet -->
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
+      rel="stylesheet"
+    />
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet" />
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+    <link
+      href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css"
+      rel="stylesheet"
+    />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet" />
+	<!-- jQuery -->
+    <script src="js/jquery.js"></script>
+	
+	<!-- Script -->
+	<script type="text/javascript">
+        $(document).ready(function () {
+
+            $("#register").click(function () {
+
+                fname = $("#fname").val();
+                lname = $("#lname").val();
+                email = $("#email").val();
+                password = $("#password").val();
+
+                $.ajax({
+                    type: "POST",
+                    url: "adduser.php",
+                    data: "fname=" + fname + "&lname=" + lname + "&email=" + email + "&password=" + password,
+                    success: function (html) {
+                        if (html == 'true') {
+
+                            $("#add_err2").html('<div class="alert alert-success"> \
+                                                 <strong>Account</strong> processed. \ \
+                                                 </div>');
+
+                            window.location.href = "index.php";
+
+                        } else if (html == 'false') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> already in system. \ \
+                                                 </div>');                    
+
+                        } else if (html == 'fname') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>First Name</strong> is required. \ \
+                                                 </div>');
+												 
+						} else if (html == 'lname') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Last Name</strong> is required. \ \
+                                                 </div>');
+
+                        } else if (html == 'eshort') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> is required. \ \
+                                                 </div>');
+
+                        } else if (html == 'eformat') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> format is not valid. \ \
+                                                 </div>');
+												 
+						} else if (html == 'pshort') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Password</strong> must be at least 4 characters . \ \
+                                                 </div>');
+
+                        } else {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Error</strong> processing request. Please try again. \ \
+                                                 </div>');
+                        }
+                    },
+                    beforeSend: function () {
+                        $("#add_err2").html("loading...");
+                    }
+                });
+                return false;
+            });
+        });
+    </script>
+
+</head>
+
+<body>
+    <div class="container-xxl bg-white p-0">
+      
+    <!-- Navigation -->
+    <?php require_once 'nav.php'; ?>
+        <div class="container-xxl position-relative p-0">
+        <div class="container-xxl py-5 bg-dark hero-header mb-5">
+          <div class="container text-center my-5 pt-5 pb-4">
+            <h1 class="display-3 text-white mb-3 animated slideInDown">
+              Register Here
+            </h1>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb justify-content-center text-uppercase">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                <li
+                  class="breadcrumb-item text-white active"
+                  aria-current="page"
+                >
+                  Registration
+                </li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <!-- Hero End -->
+    <div class="container">
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Registration
+                        <strong>form</strong>
+                    </h2>
+					<div id="add_err2"></div>
+                    <hr>       
+                    <form role="form">
+                        <div class="row">
+                            <div class="form-group col-lg-4">
+                                <label class="">First Name</label>
+                                <input type="text" id="fname" name="fname" maxlength="25" class="form-control">
+                            </div>
+                            <div class="form-group col-lg-4">
+                                <label>Last Name</label>
+                                <input type="text" id="lname" name="lname" maxlength="25" class="form-control">
+                            </div>
+                            <div class="form-group col-lg-4">
+                                <label>Email Address</label>
+                                <input type="email" id="email" name="email" maxlength="25" class="form-control">
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="form-group col-lg-12">
+                                <label>Password</label>
+                                <input type="password" id="password" name="password" maxlength="10" class="form-control">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <button type="submit" id="register" class="btn btn-primary w-100 mt-4 py-2 ">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+     <?php include_once 'footer.php' ?>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
