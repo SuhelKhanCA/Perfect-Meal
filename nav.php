a0083cc5a0e2f4c9bc97cca43c91d00c40341a4e
suhelkhanca <suhel.hata@gmail.com>
2024-11-01 22:24:57 +0530

    nav bar

--- nav.php
+++ nav.php
@@ -19,10 +19,10 @@
           </button>
           <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto py-0 pe-4">
-              <a href="index.html" class="nav-item nav-link active">Home</a>
-              <a href="about.html" class="nav-item nav-link">About</a>
-              <a href="service.html" class="nav-item nav-link">Service</a>
-              <a href="menu.html" class="nav-item nav-link">Menu</a>
+              <a href="index.php" class="nav-item nav-link active">Home</a>
+              <a href="about.php" class="nav-item nav-link">About</a>
+              <a href="service.php" class="nav-item nav-link">Service</a>
+              <a href="menu.php" class="nav-item nav-link">Menu</a>
               <div class="nav-item dropdown">
                 <a
                   href="#"
@@ -31,16 +31,16 @@
                   >Pages</a
                 >
                 <div class="dropdown-menu m-0">
-                  <a href="booking.html" class="dropdown-item">Booking</a>
-                  <a href="team.html" class="dropdown-item">Our Team</a>
-                  <a href="testimonial.html" class="dropdown-item"
+                  <a href="booking.php" class="dropdown-item">Booking</a>
+                  <a href="team.php" class="dropdown-item">Our Team</a>
+                  <a href="testimonial.php" class="dropdown-item"
                     >Testimonial</a
                   >
                 </div>
               </div>
-              <a href="contact.html" class="nav-item nav-link">Contact</a>
+              <a href="contact.php" class="nav-item nav-link">Contact</a>
             </div>
-            <a href="" class="btn btn-primary py-2 px-4">Book A Table</a>
+            <a href="booking.php" class="btn btn-primary py-2 px-4">Book A Table</a>
           </div>
         </nav>
         </div>
